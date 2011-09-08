ab972a23af436566e3f37da8266716c33e3eca58
Tiberiu Ticoan <tiberiu.ticoan@skobbler.com>
2011-09-08 17:24:50 +0300

    Adding wrapper

---
+++ wrapper/iOS/AddressHunter_V_01/Classes/AddressHunter_V_01AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AddressHunter_V_01AppDelegate.h
+//  AddressHunter_V_01
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface AddressHunter_V_01AppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ wrapper/iOS/AddressHunter_V_01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AddressHunter_V_01
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AddressHunter_V_01AppDelegate");
+    [pool release];
+    return retVal;
+}
